58a01d51d21c53dd9d86aecd234df2a0e9785c3e
Elise Chant <elise.chant@gmail.com>
2021-09-15 05:45:08 +1000

    add node app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# slack
+# Slack invite link
+
 Clean url for Slack invitations
+
+![Heroku](https://pyheroku-badge.herokuapp.com/?app=<designsystemau-slack-redirect>&style=<flat-square>)
+
+## Deploy
+
+This app deploys to Heroku on main merge, which can be configured at Heroku dashboard. 
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const { PORT=3000, LOCAL_ADDRESS='0.0.0.0', SLACK_INVITATION_LINK='https://google.com' } = process.env;
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+	res.redirect(301, SLACK_INVITATION_LINK);
+});
+
+app.listen(PORT, LOCAL_ADDRESS, () => {
+	console.log(`App listening at http://${LOCAL_ADDRESS}:${PORT}`);
+});
